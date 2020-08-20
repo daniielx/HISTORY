553c403b792a8bee90ef1f323c10c4e62bd47d72
daniielx <danielpaul.20cs@licet.ac.in>
2020-08-21 03:28:30 +0530

    added simple admin usecase page

---
+++ client/javascript/home.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("adminForm").style.display = "block";
+}
+
+function cancelForm() {
+    document.getElementById("adminForm").style.display = "none";
+}
