6c9a0c13cfece85d1fdee82dad7e83d2f3ca14da
daniielx <danielpaul.20cs@licet.ac.in>
2020-08-21 23:31:57 +0530

    added simple admin usecase page

--- client/javascript/home.js
+++ client/javascript/home.js
@@ -5,3 +5,7 @@ function openForm() {
 function cancelForm() {
     document.getElementById("adminForm").style.display = "none";
 }
+
+function setKey() {
+    const inputText = document.getElementById("answerKeyAdmin").value;
+}
